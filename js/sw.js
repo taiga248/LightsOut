f9f9e23ecdeff7ad795502882d2f055ead4c8476
Taiga <taiga248@gmail.com>
2020-08-29 19:46:34 +0900

    Update

---
+++ js/sw.js
@@ -0,0 +1,24 @@
+const cacheName = "cacheData";
+const resoucesToPreccache = ["/", "index.html", "css/style.css"];
+
+self.addEventListener("install", event => {
+  console.log("Service worker install event");
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        return cache.addAll(resoucesToPreccache);
+      })
+      .catch(error => {
+        console.log("Failed", error);
+      })
+  );
+});
+
+self.addEventListener("activate", () => {
+  console.log("Activate");
+});
+
+self.addEventListener("fetch", event => {
+  console.log("Fetch intercepted for: " + event.request.url);
+});
